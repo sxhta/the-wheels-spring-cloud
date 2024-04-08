b2cb82b81110500951f90281833c01f31491f4b4
apefuu <apefuu@gmail.com>
2024-04-08 20:05:44 +0800

    添加order

---
+++ wheels-order/src/main/java/com/wheels/cloud/order/service/user/impl/FrontUserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.wheels.cloud.order.service.user.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.sxhta.cloud.wheels.remote.domain.user.WheelsFrontUser;
+import com.sxhta.cloud.wheels.remote.request.RegisterRequest;
+import com.sxhta.cloud.wheels.remote.vo.FrontUserCacheVo;
+import com.wheels.cloud.order.mapper.user.FrontUserMapper;
+import com.wheels.cloud.order.service.user.FrontUserService;
+import org.springframework.stereotype.Service;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Service
+public class FrontUserServiceImpl extends ServiceImpl<FrontUserMapper, WheelsFrontUser> implements FrontUserService, Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public WheelsFrontUser getUserByAccount(String account) {
+        final var lqw = new LambdaQueryWrapper<WheelsFrontUser>();
+        lqw.eq(WheelsFrontUser::getUserName,account);
+        final var user = getOne(lqw);
+        return user;
+    }
+
+    @Override
+    public FrontUserCacheVo register(RegisterRequest request) {
+        final var account = request.getAccount();
+        final var wheelsFrontUser = new WheelsFrontUser();
+        //通过手机号注册的用户名默认和account 相同
+        wheelsFrontUser.setAccount(account)
+                .setUserName(account);
+        return new FrontUserCacheVo();
+    }
+}
